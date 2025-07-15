a9ef9424b6588bde0628aa784fb1d4b041a8fa26
luvchoices <moonhyungstar@gmail.com>
2025-07-15 03:34:40 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-<const malla = [
+const malla = [
   {
     semestre: "Semestre I",
     ramos: [
@@ -126,7 +126,7 @@ function toggleAprobado(el) {
       const prereqs = JSON.parse(ramo.dataset.prereq)
       const aprobados = prereqs.every((p) =>
         document.querySelector(`.ramo[data-id="${p}"]`)?.classList.contains("aprobado")
-      );
+      )
       if (aprobados) {
         ramo.classList.remove("bloqueado")
       }
